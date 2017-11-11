fecf8745a57ec32682e684525f477263da41aa12
Jonathan Dana <jonathandana27@gmail.com>
2017-11-11 21:05:41 +0200

    revision bug

---
+++ public_html/app/code/local/JD/Example/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class JD_Example_IndexController extends Mage_Core_Controller_Front_Action{
+
+    /**
+     *
+     */
+    public function indexAction(){
+        echo 'hello, word';
+    }
+}
